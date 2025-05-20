6991c29f59a51d6de2562543567df2f129c434eb
Lewis Schofield <22LSchofield@heckgrammar.co.uk>
2025-05-20 15:10:55 +0100

    ..............

--- matching game/Form1.cs
+++ matching game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,21 +22,16 @@ namespace matching_game
         }
 
 
-        // firstClicked points to the first Label control 
-        // that the player clicks, but it will be null 
-        // if the player hasn't clicked a label yet
+        
         Label firstClicked = null;
 
-        // secondClicked points to the second Label control 
-        // that the player clicks
+       
         Label secondClicked = null;
 
-        // Use this Random object to choose random icons for the squares
+       
         Random random = new Random();
 
-        // Each of these letters is an interesting icon
-        // in the Webdings font,
-        // and each icon appears twice in this list
+      
         List<string> icons = new List<string>()
     {
         "P", "P", "N", "N", ",", ",",  
@@ -43,20 +39,16 @@ namespace matching_game
 
 };
 
-        /// <summary>
-        /// Assign each icon from the list of icons to a random square
-        /// </summary>
+
         private void AssignIconsToSquares()
         {
-            // The TableLayoutPanel has 16 labels,
-            // and the icon list has 16 icons,
-            // so an icon is pulled at random from the list
-            // and added to each label
+
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
                 if (iconLabel != null)
                 {
+                    
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
                     iconLabel.ForeColor = iconLabel.BackColor;
@@ -81,10 +73,7 @@ namespace matching_game
         }
 
         private void label1_Click(object sender, EventArgs e)
-        {
-            // The timer is only on after two non-matching 
-            // icons have been shown to the player, 
-            // so ignore any clicks if the timer is running
+        { 
             if (timer1.Enabled == true)
                 return;
 
@@ -92,16 +81,11 @@ namespace matching_game
 
             if (clickedLabel != null)
             {
-                // If the clicked label is black, the player clicked
-                // an icon that's already been revealed --
-                // ignore the click
+       
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                // If firstClicked is null, this is the first icon
-                // in the pair that the player clicked, 
-                // so set firstClicked to the label that the player 
-                // clicked, change its color to black, and return
+         
                 if (firstClicked == null)
                 {
                     firstClicked = clickedLabel;
@@ -109,20 +93,15 @@ namespace matching_game
                     return;
                 }
 
-                // If the player gets this far, the timer isn't
-                // running and firstClicked isn't null,
-                // so this must be the second icon the player clicked
-                // Set its color to black
+
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
 
-                // Check to see if the player won
+          
                 CheckForWinner();
 
-                // If the player clicked two matching icons, keep them 
-                // black and reset firstClicked and secondClicked 
-                // so the player can click another icon
+ 
                 if (firstClicked.Text == secondClicked.Text)
                 {
                     firstClicked = null;
@@ -131,10 +110,7 @@ namespace matching_game
                 }
 
 
-                // If the player gets this far, the player 
-                // clicked two different icons, so start the 
-                // timer (which will wait three quarters of 
-                // a second, and then hide the icons)
+               
                 timer1.Start();
             }
 
@@ -143,24 +119,21 @@ namespace matching_game
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            // Stop the timer
+      
             timer1.Stop();
 
-            // Hide both icons
+           
             firstClicked.ForeColor = firstClicked.BackColor;
             secondClicked.ForeColor = secondClicked.BackColor;
 
-            // Reset firstClicked and secondClicked 
-            // so the next time a label is
-            // clicked, the program knows it's the first click
+
             firstClicked = null;
             secondClicked = null;
         }
 
         private void CheckForWinner()
         {
-            // Go through all of the labels in the TableLayoutPanel, 
-            // checking each one to see if its icon is matched
+         
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
@@ -172,11 +145,33 @@ namespace matching_game
                 }
             }
 
-            // If the loop didn’t return, it didn't find
-            // any unmatched icons
-            // That means the user won. Show a message and close the form
-            MessageBox.Show("You matched all the icons! YOU WIN!!!", "Congratulations");
-            Close();
+           
+            
+            
+            DialogResult dialogResult = MessageBox.Show("REPLAY?", "PLAY AGAIN", MessageBoxButtons.YesNoCancel);
+            if (dialogResult == DialogResult.Yes)
+            {
+               
+                InitializeComponent();
+                AssignIconsToSquares();
+                
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                Close();
+            }
+            else if (dialogResult == DialogResult.Cancel)
+            {
+               DialogResult dialogResult1 = MessageBox.Show("HOW DARE YOU CLICK CANCEL", "NEVER CANCEL MY GAME", MessageBoxButtons.OKCancel);
+                if(dialogResult1 == DialogResult.Cancel)
+                {
+                    MessageBoxIcon iconerror = MessageBoxIcon.Error;
+                    
+                    
+                }
+            }
+
+           
         }
     }
 }
